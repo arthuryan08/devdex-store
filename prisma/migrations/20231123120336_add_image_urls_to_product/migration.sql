a11d7dc5e8dbacbb3442f88209adebeea95477cb
arthuryan08 <arthuryan08@gmail.com>
2023-11-23 09:35:33 -0300

    feat: add seed script to prisma, imageUrls and discountPercentage changed to product table

---
+++ prisma/migrations/20231123120336_add_image_urls_to_product/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Products` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Products" DROP CONSTRAINT "Products_categoryId_fkey";
+
+-- DropTable
+DROP TABLE "Products";
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "imageUrls" TEXT[],
+    "categoryId" TEXT NOT NULL,
+    "discountPercent" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
