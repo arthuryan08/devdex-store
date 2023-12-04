3501afacaee04e15d9d1f58b42dc9033c4e37a15
arthuryan08 <arthuryan08@gmail.com>
2023-12-03 23:43:27 -0300

    feat: add Deals Page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -101,10 +101,17 @@ export const Header = () => {
                 </Link>
               </SheetClose>
 
-              <Button variant="outline" className="w-full justify-start gap-2">
-                <Percent size={16} />
-                Ofertas
-              </Button>
+              <SheetClose asChild>
+                <Link href={`/deals`}>
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <Percent size={16} />
+                    Ofertas
+                  </Button>
+                </Link>
+              </SheetClose>
 
               <SheetClose asChild>
                 <Link href={`/catalog`}>
